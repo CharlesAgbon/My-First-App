b67a536459b01073a0359b2e11f6be3bfbec1f3e
agboncharlesroland@gmail.com <agboncharlesroland@gmail.com>
2018-07-16 12:22:46 +0800

    homework

---
+++ app/src/main/java/edu/sti/myfirstapp/MyActivity.java
@@ -0,0 +1,20 @@
+package edu.sti.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+
+}
